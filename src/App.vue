20d1c40a84d0694c3f422ba6c6a292a0354160e8
pastel-heat <ethamilton@outlook.com>
2023-04-11 13:27:40 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="app">
+
+    <h1>Would You Rather?</h1>
+
+    <!-- bind props in child component to data in app.vue, and call method when receiving an answer-changed event from child -->
+    <would-you-rather 
+    v-bind:question="wyrQuestion"
+    v-bind:answer1="wyrAnswer1"
+    v-bind:answer2="wyrAnswer2"
+    v-on:answer-changed="answerChanged">
+    </would-you-rather>
+
+    <p>{{ userSelectMessage }}</p>
+  </div>
+</template>
+
+<script>
+//import child component
+import WouldYouRather from './components/WouldYouRather.vue'
+
+export default {
+  name: 'App',
+  components: {
+    WouldYouRather
+  },
+  data() {
+    return {
+      wyrQuestion: "Would you rather have the ability to see 10 minutes into the future or 150 years into the future?",
+      wyrAnswer1: "10 minutes",
+      wyrAnswer2: "150 years",
+      userSelectMessage: ""
+    }
+  },
+  methods: {
+    answerChanged(choice) {
+      this.userSelectMessage = "Thanks, you chose " + choice
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/WouldYouRather.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="wyr">
+    
+    <h2>Please make your choice.</h2>
+
+    <h3>{{ question }}</h3>
+
+    <!-- radio buttons bound to wyr answers, and modeled to choice in data-->
+    <input type="radio" v-model="choice" v-bind:value="answer1" v-on:change="choiceMade">
+    <label>{{ answer1 }}</label>
+
+    <input type="radio" v-model="choice" v-bind:value="answer2" v-on:change="choiceMade">
+    <label>{{ answer2 }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WouldYouRather',
+  props: {
+    question: String,
+    answer1: String,
+    answer2: String
+  },
+  data() {
+    return {
+      choice: ''
+    }
+  },
+  methods: {
+    choiceMade() {
+      //send event to parent when radio button selection is changed. include current choice w/ event
+      this.$emit('answer-changed', this.choice)
+    }
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+.wyr {
+  border: 2px solid black;
+  background-color: skyblue;
+  color: black;
+}
+
+
+</style>
